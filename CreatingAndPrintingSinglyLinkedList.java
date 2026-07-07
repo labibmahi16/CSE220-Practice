eeadf52a222266950b40c5051e48f2b813e646ab
K.M Mahir Labib <labibmahi7@gmail.com>
2026-07-07 17:02:12 +0600

    Creating And Printing Singly LinkedList

---
+++ CreatingAndPrintingSinglyLinkedList.java
@@ -0,0 +1,40 @@
+public class CreatingAndPrintingSinglyLinkedList{
+
+  public static void main(String[] args) {
+
+        Node n1 = new Node(58);
+        Node n2 = new Node(25);
+        Node n3 = new Node(16);
+        Node n4 = new Node(28);
+        Node n5 = new Node(70);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        Node current = n1;
+        while (current != null) {
+            System.out.print(current.elem + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+class Node{
+
+    Object elem;
+    Node next;
+
+    public Node(Object elem, Node next){
+        this.elem = elem;
+        this.next = next;
+    }
+    
+    //Overloading
+    public Node(Object elem){
+        this.elem = elem;
+        this.next = null;
+    }
+}
